36aaa7c247182053901a161cbc06d6f9ce27669e
Grégory Bataille <gregory.bataille@gmail.com>
2019-12-13 07:16:56 +0100

    12 part 2 with large solution

--- day12.py
+++ day12.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from enum import Enum
 from input_utils import get_input
 from log_utils import log
+import math
 import re
 from typing import List, Set
 
@@ -18,6 +19,28 @@ class Vector:
     dx: int
     dy: int
     dz: int
+
+
+@dataclass(frozen=True)
+class CoordState:
+    positions: List[int]
+    velocities: List[int]
+
+    @staticmethod
+    def from_space(space: 'Space', coord: str) -> 'CoordState':
+        pos = []
+        vel = []
+        for moon in space.moons:
+            pos.append(getattr(moon.position, coord))
+            vel.append(getattr(moon.velocity, f'd{coord}'))
+
+        return CoordState(pos, vel)
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    def __hash__(self):
+        return hash(tuple(self.positions)) * hash(tuple(self.velocities))
 
 
 @dataclass
@@ -56,16 +79,21 @@ class SpaceTime:
     state: List[Space]
 
 
-def time_step(space_time: SpaceTime) -> SpaceTime:
-    apply_gravity_force(space_time)
-    move_moons(space_time)
-
-    return space_time
+def lcm(a, b):
+    return abs(a*b) // math.gcd(a, b)
 
 
-def apply_gravity_force(space_time: SpaceTime) -> SpaceTime:
-    last_state = space_time.state[-1]
+def time_step(space: Space) -> Space:
+    space = apply_gravity_force(space)
+    space = move_moons(space)
+
+    return space
+
+
+def apply_gravity_force(last_state: Space) -> Space:
+    new_moons = []
     for moon in last_state.moons:
+        v = Vector(moon.velocity.dx, moon.velocity.dy, moon.velocity.dz)
         for other in last_state.moons:
             if other == moon:
                 continue
@@ -73,59 +101,113 @@ def apply_gravity_force(space_time: SpaceTime) -> SpaceTime:
             for coord in ['x', 'y', 'z']:
                 delta = getattr(other.position, coord) - getattr(moon.position, coord)
                 if delta != 0:
-                    setattr(moon.velocity, f'd{coord}', getattr(moon.velocity, f'd{coord}') + int(delta / abs(delta)))
+                    setattr(v, f'd{coord}', getattr(v, f'd{coord}') + int(delta / abs(delta)))
 
-    return space_time
-
-
-def move_moons(space_time: SpaceTime) -> SpaceTime:
-    last_state = space_time.state[-1]
-    new_moons = []
-    for moon in last_state.moons:
         new_moons.append(
             Moon(
                 Position(
-                    moon.position.x + moon.velocity.dx,
-                    moon.position.y + moon.velocity.dy,
-                    moon.position.z + moon.velocity.dz
+                    moon.position.x,
+                    moon.position.y,
+                    moon.position.z
                 ),
-                moon.velocity
+                v
             )
         )
 
-    space_time.state.append(Space(new_moons))
-    return space_time
+    return Space(new_moons)
 
 
-def parse_input(input_str: str) -> SpaceTime:
+def move_moons(last_state: Space) -> Space:
+    for moon in last_state.moons:
+        moon.position.x = moon.position.x + moon.velocity.dx
+        moon.position.y = moon.position.y + moon.velocity.dy
+        moon.position.z = moon.position.z + moon.velocity.dz
+
+    return last_state
+
+
+def parse_input(input_str: str) -> Space:
     moons = []
     for moon_str in input_str.split('\n'):
         m = re.match(r'<x=(.*), y=(.*), z=(.*)>', moon_str)
         moons.append(Moon(Position(int(m.group(1)), int(m.group(2)), int(m.group(3)))))
 
-    space = Space(moons)
-    return SpaceTime([space])
+    return Space(moons)
 
 
 if __name__ == '__main__':
     input_str = get_input('12')
-    input_str = """<x=-1, y=0, z=2>
-<x=2, y=-10, z=-7>
-<x=4, y=-8, z=8>
-<x=3, y=5, z=-1>"""
+#     input_str = """<x=-1, y=0, z=2>
+# <x=2, y=-10, z=-7>
+# <x=4, y=-8, z=8>
+# <x=3, y=5, z=-1>"""
+#     input_str = """<x=-8, y=-10, z=0>
+# <x=5, y=5, z=10>
+# <x=2, y=-7, z=3>
+# <x=9, y=-8, z=-3>"""
     log(input_str)
 
-    space_time = parse_input(input_str)
-    log(space_time)
+    space0 = parse_input(input_str)
+    space = parse_input(input_str)
 
     nb_steps = 1000
 
-    iteration = 1
+    iteration = 0
+    x_state = set()
+    y_state = set()
+    z_state = set()
+
+    x_cycle_size = None
+    y_cycle_size = None
+    z_cycle_size = None
+
+    x = CoordState.from_space(space, 'x')
+    y = CoordState.from_space(space, 'y')
+    z = CoordState.from_space(space, 'z')
+
+    print('############')
+    print(x)
+    print(y)
+    print(z)
+    print('############')
+
+    x_state.add(x)
+    y_state.add(y)
+    z_state.add(z)
+
     while True:
+    # for _ in range(nb_steps):
         iteration += 1
-        space_time = time_step(space_time)
-        if space_time.state[0] == space_time.state[-1]:
+        space = time_step(space)
+
+        x = CoordState.from_space(space, 'x')
+        y = CoordState.from_space(space, 'y')
+        z = CoordState.from_space(space, 'z')
+
+
+        if x in x_state and not x_cycle_size:
+            x_cycle_size = iteration
+            print(x)
+        else:
+            x_state.add(x)
+        if y in y_state and not y_cycle_size:
+            y_cycle_size = iteration
+            print(y)
+        else:
+            y_state.add(y)
+        if z in z_state and not z_cycle_size:
+            z_cycle_size = iteration
+            print(z)
+        else:
+            z_state.add(z)
+
+        if x_cycle_size and y_cycle_size and z_cycle_size:
+            print('found all cycles')
+            print(x_cycle_size)
+            print(y_cycle_size)
+            print(z_cycle_size)
+            print(lcm(lcm(x_cycle_size, y_cycle_size), z_cycle_size))
             break
 
-
-    print(iteration)
+        if space0 == space:
+            break
