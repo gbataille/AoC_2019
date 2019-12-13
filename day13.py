11e47eabacaebbfdda0bf17ffae304fb6f2407fd
Grégory Bataille <gregory.bataille@gmail.com>
2019-12-13 21:16:23 +0100

    13 with UI

--- day13.py
+++ day13.py
@@ -3,7 +3,22 @@ from enum import Enum
 from input_utils import get_input
 from intcode import Program, read_memory, run_program
 from log_utils import log
+import os
+import time
 from typing import List, Set
+
+
+def render(value: int):
+    if value == 0:
+        return ' '
+    if value == 1:
+        return '-'
+    if value == 2:
+        return 'X'
+    if value == 3:
+        return '_'
+    if value == 4:
+        return 'O'
 
 
 @dataclass
@@ -38,10 +53,14 @@ class Scene:
             raise
 
     def show(self):
+        os.system('clear')
+        print(f'Score: {self.score}')
         for line_idx in range(self.max_y + 1):
             line = self.tiles[line_idx]
             cols = [line[i] for i in range(self.max_x + 1)]
-            print(' '.join(list(map(str, cols))))
+            print(' '.join(list(map(render, cols))))
+
+        time.sleep(0.02)
 
     def count_blocks(self):
         count = 0
@@ -81,9 +100,11 @@ def update_scene(scene: Scene) -> None:
         else:
             if scene.next_x == -1 and scene.next_y == 0:
                 scene.score = value
-                print(f'New score: {scene.score}')
+                scene.show()
             else:
                 scene.set_value(scene.next_x, scene.next_y, value)
+                if value == 4:
+                    scene.show()
 
             scene.next_x = None
             scene.next_y = None
@@ -105,5 +126,3 @@ if __name__ == '__main__':
         update_scene(scene)
     )
     program = run_program(program)
-    scene.show()
-    print(scene.count_blocks())
