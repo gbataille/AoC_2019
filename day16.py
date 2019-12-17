fac0e85914cc9a5f662cad88d0a06bb7f51bc41b
Grégory Bataille <gregory.bataille@gmail.com>
2019-12-17 06:32:32 +0100

    16 part 2

--- day16.py
+++ day16.py
@@ -48,10 +48,30 @@ def signal_from_input(input_str: str) -> Signal:
 
 if __name__ == '__main__':
     input_str = get_input('16')
-    # input_str = '80871224585914546619083218645595'
+    # input_str = '03081770884921959731165446850517'
     signal = signal_from_input(input_str)
     signal = Signal(signal.data * NB_INPUT_REPETITION)
-    for _ in range(NB_PHASES):
-        signal = run_phase(signal)
 
-    print(''.join(list(map(str, signal.data[:8]))))
+    # For part 2 there is a trick.
+    # An element in the output at position N does not depend on anyting before position N is the
+    # input list, because of the leading 0s in the repeating pattern
+    # * You don't compute anything before the offset.
+    # Then you realize that len(input_list) - offset < offset
+    # Which means that the mask is 0 till the offset and then 1 till the end.
+    # * You just have to do a sum
+    # * The smart way to do it is that the last element in a phase is the last element is the phase
+    # before. The penultimate element in phase P is last element in phase P + penultimate element in
+    # phate P - 1
+
+    offset = int(input_str[:7])
+    input_len = len(input_str) * 10000
+    log(f'Offset {offset}, input_len {input_len}')
+
+    stripped_input = signal.data[offset:]
+    for _ in range(NB_PHASES):
+        new_signal = [0 for _ in range(len(stripped_input))]
+        for i in range(len(stripped_input)):
+            new_signal[i*-1 - 1] = (new_signal[i*-1] + stripped_input[i*-1 - 1]) % 10
+        stripped_input = new_signal
+
+    print(''.join(list(map(str, stripped_input[:8]))))
